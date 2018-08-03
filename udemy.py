e70b5e5cdfac049dcc05572426597a039454a0aa
wonikjang <wj2216@columbia.edu>
2018-08-03 15:43:40 +0900

    Update udemy.py

--- udemy.py
+++ udemy.py
@@ -1,16 +1,13 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Aug  2 23:11:05 2018
+Created on Fri Aug  3 09:18:59 2018
 
-@author: wonikJang
+@author: 09732
 """
 
-# Udemy 
-
-# Intro - find 3 elements that multiply to 100 
-
 arr = [1,2,4,5,10,20,50]
+
+
 
 # ============ 1. Most Frequently Occuring Item  : O(n)
 
@@ -117,11 +114,10 @@ def is_rotation(A,B):
     return True
     
 
-
 is_rotation(a,b)
 
 
-# ========= 4. Non Repeating chara return 
+# ========= 4. Non Repeating chara return : O(n)
 
 
 def single_char(given_array):
@@ -149,6 +145,147 @@ def single_char(given_array):
 single_char([1,1,2])
 
 
+# ========= 5. One Away Strings : O(n)
+
+# Given 2 strings, True / False whether one awaw : 
+# 1. Changed (including the same) 2. Eat(One is removed ) 3. Add (One is more )
+
+s1 = 'abcde'
+s2 = 'abde'
+
+
+def one_differ( s1, s2 ):
+    
+    p1 = 0 
+    p2 = 0
+    count = 0 
+    
+    while p1 < len(s1) and p2 < len(s2):
+    
+        # Regarding Cases 
+        
+        # Difference in length > 2
+        if abs( len(s1) - len(s2) ) >= 2 : 
+            return False 
+        
+        # Same length --> move index sequentially 
+        elif abs( len(s1) - len(s2) ) == 0:
+            
+            if s1[p1] != s2[p2]:
+                count += 1 
+            
+            p1 += 1
+            p2 += 1
+            
+        # Difference in Length == 1 
+        else:
+            
+            if ( len(s1) - len(s2) ) > 0:    
+                longer_str = s1
+                shorter_str = s2 
+            else:
+                longer_str = s2
+                shorter_str = s1
+            
+            if longer_str[p1] != shorter_str[p2]:
+                count += 1
+                p1 += 1
+        
+            p1 += 1
+            p2 += 1
+            
+        # Regarding Count 
+        if count > 1:
+            return False 
+
+    return True
+
+one_differ(s1, s2)
+
+
+
+
+# =========== 6. Assign Number in Minesqwwper 
+
+
+import numpy as np
+
+a = np.array([[-1,2,3],[3,4,5]])
+
+ls = [[0,1],[0,2]]
+
+b = np.pad(a, ((1,1),(1,1)), 'constant')
+
+#np.pad(a, 1, 0)
+
+def surround(arr, n, m ):
+    
+    count = 0
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            
+            if i ==0 and j ==0:
+                continue
+            
+            focal = arr[n + i, m +j ]
+            if focal == -1:
+                count += 1
+                
+    return count             
+    
+surround(b, 2, 2 )  
+
+
+def mine_sweeper( bombs, num_row, num_col):
+    
+    # === Data Generation 
+    arr = np.zeros([num_row, num_col])
+
+    # Assign Bomb into the array 
+    for item in bombs:
+        arr[item[0], item[1] ] = -1 
+        
+    # === Padding 
+    
+    arr1 = np.pad(arr, ((1,1),(1,1)), 'constant')
+
+    # === loop through  
+    
+    for row in range( 1, num_row + 1 ):
+        for col in range( 1, num_col + 1 ):
+            
+            # Jump Bomb 
+            if arr1[row, col] == -1: 
+                continue
+            
+            # 
+            else: 
+                count = surround(arr1, row, col )
+                arr1[row, col] = count
+
+    # Remove Zero Padding 
+    arr2 = arr1[ 1 : arr.shape[0] + 1 ,  1 : arr.shape[1] + 1,   ]
+
+    return arr2
+
+arr2 = mine_sweeper( [[0,0],[0,1]], 3, 4) 
+# [[-1,-1,1,0],[2,2,1,0] ,[0,0,0,0] ]
+
+# ====== 7. Find Where to expand in MineSweeper 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # ======== Daily Coding Problem 3 (Google)
 
 class Node:
@@ -162,9 +299,6 @@ node = Node('root', Node('left', Node('left.left')), Node('right') )
 
 assert deserialize(serialize(node)) 
 left.left.val == 'left.left'
-
-
-
 
 
 
